05162cb7224555c8c4073ea7c84c0bbb176e3059
Vinay Singh <vinaysthakur15@gmail.com>
2021-08-31 23:51:07 +0530

    Practice

---
+++ Strings/30Aug2021/StringSorting.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+bool compare(string a , string b)
+{
+  // Default way of comparing
+
+  // a<b;
+
+  // Comparing wrt to length of strings
+
+  if(a.length()==b.length()){
+    // lexograph. increasing
+
+    return a<b;
+
+    // lexograph. decreasing
+    // return a>b;
+  }
+  // for increasing sort
+
+  return a.length()<b.length();
+
+  // for decreasing sort
+
+  // return a.length()>b.length();
+
+}
+
+int main()
+{
+  int n;
+  cin>>n;
+  cin.get();
+
+  string s[100]; //or we can use vector instead too
+
+
+  for(int i =0;i<n;i++)
+  {
+    // overloaded with different arguments
+
+    getline(cin,s[i]) ;
+    
+  }
+
+  sort(s,s+n,compare);
+
+  for(int i =0;i<n;i++)
+  {
+    cout << s[i]<<endl;
+  }
+
+
+  
+
+  
+}
