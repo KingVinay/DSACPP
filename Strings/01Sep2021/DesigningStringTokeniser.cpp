33a5cadfb1262faafae20494e34fe2a7cbd07d76
Vinay Singh <vinaysthakur15@gmail.com>
2021-09-11 20:26:06 +0530

    Practice

---
+++ Strings/01Sep2021/DesigningStringTokeniser.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+//input of mystrtok() is string and a single char as delimiter
+
+char * mystrtok(char *str,char delim)
+{
+  
+  static char *input = NULL;
+  if(str!=NULL)
+  {
+    //We are making our first call
+    input = str;
+
+  }
+
+  //Check here base case after the final token has been returned
+  if(input==NULL)
+  {
+    return NULL;
+  }
+  
+  //Start extracting tokens and store them in an array
+
+  char *output = new char[strlen(input)+1]; //dynamic array creation
+  
+  int i=0;
+  for(;input[i]!='\0';i++ )
+  {
+    if(input[i]!=delim)
+    {
+    output[i]=input[i];
+    }
+    else
+    {
+      output[i]='\0';
+      input = input + i + 1;
+      return output;
+
+    }
+
+  }
+  //Corner Case (At last index it might not have delim so)
+
+  output[i]='\0';
+  input = NULL;
+  return output;
+
+
+
+}
+
+int main()
+{
+  char s[100]="Today, is a, rainy day";
+
+  char *ptr = mystrtok(s,',');
+  cout<<ptr<<endl;
+
+  while(ptr!=NULL)
+  {
+    ptr = mystrtok(NULL,',');
+    cout<<ptr<<endl;
+  }
+  return 0;
+
+}
