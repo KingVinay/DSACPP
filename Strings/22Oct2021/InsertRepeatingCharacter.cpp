1f5c416d21a987e45fe4b657cb031a7e952880e5
Vinay Singh <vinaysthakur15@gmail.com>
2021-10-22 23:30:55 +0530

    Practice

---
+++ Strings/22Oct2021/InsertRepeatingCharacter.cpp
@@ -0,0 +1,51 @@
+// Solution for Inserting Repeating Character in the middle of an string and return an dynamic array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+char* repeatcharacter(char str[],char c,int n)
+{
+    string s(str);
+    int m = s.length(),count=0;
+    if(m==0)
+    {
+        return NULL;
+    }
+    
+    char* a = new char[m+n];
+    
+    for(int i=0;i<m/2;i++)
+    {
+        a[count++]=s[i];
+        
+    }
+    
+    while(n>0)
+    {
+        a[count++]= c;
+        n--;
+    }
+    
+    for(int i=m/2;i<m;i++)
+    {
+        a[count++] = s[i];
+    }
+    
+    return a;
+    
+}
+
+int main()
+{
+    int n;
+    char c;
+    cin>>n>>c;
+    char a[100]={};
+    
+    char * ptr = repeatcharacter(a,c,n);
+    if(ptr==NULL){cout<<"Empty String";}
+    cout<<ptr;
+    
+    return 0;
+    
+}
